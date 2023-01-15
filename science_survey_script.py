c39481df84c68b828504e95d8b5a1f99d8eb912c
Alvin-Gavel <alvin.gavel@gmail.com>
2023-01-15 15:51:23 +0100

    Added handling of never-to-always questions

--- science_survey_script.py
+++ science_survey_script.py
@@ -17,8 +17,9 @@ n_members = {}
 for dataset_name, dataset in datasets.items():
    n_members[dataset_name] = len(set(dataset['Student ID']))
 
+question_sets = {'One to five scale': ['Survey_UnderstoodInstructions', 'Survey_LikeInternetbasedLearning', 'Survey_ILearnedALot', 'Survey_GoodLevelOfDifficulty', 'Survey_LikedTheFeedback', 'Survey_QBLLearning', 'Survey_QBLMotivation', 'Survey_MoreQBLCourses'],
+'Never to always scale': ['Survey_SearchingForAnswers', 'Survey_FirstTry', 'Survey_ContinuedToRespond', 'Survey_Frustration']}
 
-one_to_five_questions = ['Survey_UnderstoodInstructions', 'Survey_LikeInternetbasedLearning', 'Survey_ILearnedALot', 'Survey_GoodLevelOfDifficulty', 'Survey_LikedTheFeedback', 'Survey_QBLLearning', 'Survey_QBLMotivation', 'Survey_MoreQBLCourses']
 
 # To clarify what goes on here: There is no column in the data from OLI Torus that
 # actually tells us what answer a user has given. Instead, we go digging in the column
@@ -31,31 +32,38 @@ digit_regex =  re.compile('[1-5]')
 
 responses = {}
 errors = {}
-for question in one_to_five_questions:
-   responses[question] = {}
-   errors[question] = {}
-   for dataset_name, dataset in datasets.items():
-      entries = dataset[dataset['Activity Title'] == question]
+for question_set_name, question_set in question_sets.items():
+   for question in question_set:
+      responses[question] = {}
+      errors[question] = {}
+      for dataset_name, dataset in datasets.items():
+         entries = dataset[dataset['Activity Title'] == question]
       
-      responses[question][dataset_name] = np.zeros(5)
-      for response in entries['Feedback']:
-         match = response_regex.findall(response)
+         responses[question][dataset_name] = np.zeros(5)
+         for response in entries['Feedback']:
+            match = response_regex.findall(response)
          
-         if len(match) == 1:
-            digit = int(digit_regex.findall(match[0])[0])
-            responses[question][dataset_name][digit - 1] += 1
+            if len(match) == 1:
+               digit = int(digit_regex.findall(match[0])[0])
+               responses[question][dataset_name][digit - 1] += 1
       
-      errors[question][dataset_name] = np.sqrt(responses[question][dataset_name]) / n_members[dataset_name]
-      responses[question][dataset_name] /= n_members[dataset_name]
+         errors[question][dataset_name] = np.sqrt(responses[question][dataset_name]) / n_members[dataset_name]
+         responses[question][dataset_name] /= n_members[dataset_name]
 
-   plt.clf()
-   x_qbl = np.arange(1, 6) - 0.20
-   plt.bar(x_qbl, responses[question]['QBL'], width = 0.4)
-   plt.errorbar(x_qbl, responses[question]['QBL'], yerr=errors[question]['QBL'], fmt='none', ecolor='k', capsize = 5)
-   x_pqbl = np.arange(1, 6) + 0.20
-   plt.bar(x_pqbl, responses[question]['pQBL'], width = 0.4)
-   plt.errorbar(x_pqbl, responses[question]['pQBL'], yerr=errors[question]['pQBL'], fmt='none', ecolor='k', capsize = 5)
-   plt.ylabel('Responses')
-   plt.title(question)
-   plt.tight_layout()
-   plt.savefig('Article_plots/Responses_{}.png'.format(question))
+      plt.clf()
+      x_qbl = np.arange(1, 6) - 0.20
+      plt.bar(x_qbl, responses[question]['QBL'], width = 0.4)
+      plt.errorbar(x_qbl, responses[question]['QBL'], yerr=errors[question]['QBL'], fmt='none', ecolor='k', capsize = 5)
+      x_pqbl = np.arange(1, 6) + 0.20
+      plt.bar(x_pqbl, responses[question]['pQBL'], width = 0.4)
+      plt.errorbar(x_pqbl, responses[question]['pQBL'], yerr=errors[question]['pQBL'], fmt='none', ecolor='k', capsize = 5)
+      plt.ylabel('Responses')
+      plt.title(question)
+      if question_set_name == 'Never to always scale':
+         plt.xticks(ticks = np.arange(1, 6), labels = ['Never', 'Rarely', 'Sometimes', 'Often', 'Always'], rotation=90)
+      
+      plt.tight_layout()
+      plt.savefig('Article_plots/Responses_{}.png'.format(question))
+
+
+
