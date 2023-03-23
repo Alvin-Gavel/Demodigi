4e8ff91eda6bc63793404db389a1149966285cde
Alvin-Gavel <alvin.gavel@gmail.com>
2023-03-23 08:42:44 +0100

    Added variation of quality boundary

--- science_analysis_script.py
+++ science_analysis_script.py
@@ -17,3 +17,36 @@ trial_study.describe()
 trial_study.do_tests()
 trial_study.summarise_results()
 trial_study.plot_results()
+
+
+print('For comparison_testing with bounds 0.6')
+comparison_bound = fe.boundaries(0.6, 0.6, 0.1)
+
+mod = fe.real_learning_module(len(skills), 2, 'Utilities/Resultat/Artikel/IDn.json', 'Utilities/Resultat/Artikel/Individer', manipulation_path = 'Utilities/Resultat/Artikel/Manipulationer.json', boundaries = comparison_bound)
+trial_study = fe.study('Artikel', mod, plot_folder = 'Article_plots/60_percent')
+trial_study.describe()
+trial_study.do_tests()
+trial_study.summarise_results()
+trial_study.plot_results()
+
+
+print('For comparison_testing with bounds 0.7')
+comparison_bound = fe.boundaries(0.7, 0.7, 0.1)
+
+mod = fe.real_learning_module(len(skills), 2, 'Utilities/Resultat/Artikel/IDn.json', 'Utilities/Resultat/Artikel/Individer', manipulation_path = 'Utilities/Resultat/Artikel/Manipulationer.json', boundaries = comparison_bound)
+trial_study = fe.study('Artikel', mod, plot_folder = 'Article_plots/70_percent')
+trial_study.describe()
+trial_study.do_tests()
+trial_study.summarise_results()
+trial_study.plot_results()
+
+
+print('For comparison_testing with bounds 0.8')
+comparison_bound = fe.boundaries(0.8, 0.8, 0.1)
+
+mod = fe.real_learning_module(len(skills), 2, 'Utilities/Resultat/Artikel/IDn.json', 'Utilities/Resultat/Artikel/Individer', manipulation_path = 'Utilities/Resultat/Artikel/Manipulationer.json', boundaries = comparison_bound)
+trial_study = fe.study('Artikel', mod, plot_folder = 'Article_plots/80_percent')
+trial_study.describe()
+trial_study.do_tests()
+trial_study.summarise_results()
+trial_study.plot_results()
